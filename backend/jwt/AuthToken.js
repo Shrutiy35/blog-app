b58534698a6cf9faa91d474ae644d61e8191229f
Shrutiy35 <shrutiy817@gmail.com>
2025-02-25 00:39:15 +0530

    commiting the changes

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -6,7 +6,7 @@ const createTokenAndSaveCookies = async (userId, res) => {
     expiresIn: "30d",
   });
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing
+    httpOnly:true, // Temporarily set to false for testing
     secure: false,
     sameSite: "lax",
     path: "/", // Ensure the cookie is available throughout the site
