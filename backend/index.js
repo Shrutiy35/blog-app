36f29720edc59882fa247c89cd1dca9c96a39606
Shrutiy35 <shrutiy817@gmail.com>
2025-03-19 23:28:55 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      "https://your-frontend-url.onrender.com",
-    ],
+    origin: [process.env.FRONTEND_URL], // Add your deployed frontend URL here
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
